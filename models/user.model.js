0781b38e9246c472b25249da08c4b8ddf457932a
Mohamed Khaled <themidozedan@gmail.com>
2020-06-08 22:40:44 +0200

    Update user.model.js

--- models/user.model.js
+++ models/user.model.js
@@ -39,7 +39,7 @@ User.create = (userData, result) => {
     }).then(res => {
         let query = {
             name:"Create User",
-            text : "Insert into users (fullName,email,password,avatarurl) values ($1,$2,$3,$4)",
+            text : "Insert into users (fullName,email,password,avatarurl) values ($1,$2,$3,$4)  RETURNING id",
             values:[userData.fullName,userData.email,userData.password,userData.avatarURL]
         }
         sql.query(query, (err, res) => {
